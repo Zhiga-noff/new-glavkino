03453b200fddf82cc90c030d2072db207c9eed92
ms0ur <maksimsuslov7142@gmail.com>
2024-07-30 15:27:52 +0300

    Делаю адаптив для страниц павилонов и площадок

--- src/js/pavilioni.js
+++ src/js/pavilioni.js
@@ -9,10 +9,19 @@ const button6 = document.getElementById('b6');
 
 const buttons = [button1, button2, button3, button4, button5, button6];
 
+const texts = [
+	document.getElementById('t1'),
+	document.getElementById('t2'),
+	document.getElementById('t3'),
+	document.getElementById('t4'),
+	document.getElementById('t5'),
+	document.getElementById('t6'),
+];
+
 const initialChoosed = () => {
 	choosedContainer.innerHTML = `
         <h3 class="main__choosed-header">Павильон 1</h3>
-        <p class="main__choosed-text">Кино-телекомплекс включает в себя 9 съемочных павильонов от 133м2 до 3082м2. Павильоны отвечают всем требованиям индустрии кино и телевидения. Каждый павильон оборудован автономной системой кондиционирования.</p>
+        <p class="main__choosed-text">${texts[0].textContent}</p>
         `;
 	choosedContainer.style.background = `url('/img/pavilioni/1.png') center center / cover no-repeat`;
 	choosedContainer.style.backgroundBlendMode = 'multiply';
@@ -21,15 +30,32 @@ const initialChoosed = () => {
 initialChoosed();
 
 buttons.forEach((button) => {
-	button.addEventListener('click', () => {
-		choosedContainer.innerHTML = `
+	if (window.innerWidth > 992) {
+		button.addEventListener('click', () => {
+			choosedContainer.innerHTML = `
         <h3 class="main__choosed-header">Павильон ${button.id.slice(1)}</h3>
-        <p class="main__choosed-text">Кино-телекомплекс включает в себя 9 съемочных павильонов от 133м2 до 3082м2. Павильоны отвечают всем требованиям индустрии кино и телевидения. Каждый павильон оборудован автономной системой кондиционирования.</p>
+        <p class="main__choosed-text">${texts[button.id.slice(1) - 1].textContent}</p>
         `;
-		choosedContainer.style.background = `url('/img/pavilioni/${button.id.slice(1)}.png') center center / cover no-repeat`;
-		choosedContainer.style.backgroundBlendMode = 'multiply';
-		choosedContainer.style.backgroundColor = 'rgba(28, 28, 28, 0.6)';
-	});
+			choosedContainer.style.background = `url('/img/pavilioni/${button.id.slice(1)}.png') center center / cover no-repeat`;
+			choosedContainer.style.backgroundBlendMode = 'multiply';
+			choosedContainer.style.backgroundColor = 'rgba(28, 28, 28, 0.6)';
+		});
+	} else {
+		button.style.maxHeight = '8rem';
+		//accordion
+		button.addEventListener('click', () => {
+			//check if containers height is already max height
+			if (button.style.maxHeight != '8rem') {
+				button.style.maxHeight = '8rem';
+				button.style.height = '100%';
+				texts[button.id.slice(1) - 1].style.display = 'none';
+			} else {
+				texts[button.id.slice(1) - 1].style.display = 'block';
+				button.style.height = 'auto';
+				button.style.maxHeight = '28.994375rem';
+			}
+		});
+	}
 });
 document.querySelector('.headerL__back-link').addEventListener('click', () => {
 	window.history.back();
--- src/js/ploshadki.js
+++ src/js/ploshadki.js
@@ -5,10 +5,12 @@ const button2 = document.getElementById('b2');
 
 const buttons = [button1, button2];
 
+const texts = [document.getElementById('t1'), document.getElementById('t2')];
+
 const initialChoosed = () => {
 	choosedContainer.innerHTML = `
         <h3 class="main__choosed-header">Натурная площадка 1</h3>
-        <p class="main__choosed-text">Кино-телекомплекс включает в себя 9 съемочных павильонов от 133м2 до 3082м2. Павильоны отвечают всем требованиям индустрии кино и телевидения. Каждый павильон оборудован автономной системой кондиционирования.</p>
+        <p class="main__choosed-text">${texts[0].textContent}</p>
         `;
 	choosedContainer.style.background = `url('/img/ploshadki/1.png') center center / cover no-repeat`;
 	choosedContainer.style.backgroundBlendMode = 'multiply';
@@ -17,17 +19,34 @@ const initialChoosed = () => {
 initialChoosed();
 
 buttons.forEach((button) => {
-	button.addEventListener('click', () => {
-		choosedContainer.innerHTML = `
+	if (window.innerWidth > 992) {
+		button.addEventListener('click', () => {
+			choosedContainer.innerHTML = `
         <h3 class="main__choosed-header">Натурная площадка ${button.id.slice(1)}</h3>
-        <p class="main__choosed-text">Кино-телекомплекс включает в себя 9 съемочных павильонов от 133м2 до 3082м2. Павильоны отвечают всем требованиям индустрии кино и телевидения. Каждый павильон оборудован автономной системой кондиционирования.</p>
+        <p class="main__choosed-text">${texts[button.id.slice(1) - 1].textContent}</p>
         `;
-		choosedContainer.style.background = `url('/img/ploshadki/${button.id.slice(1)}.png') center center / cover no-repeat`;
-		//затемнить фон
-		choosedContainer.style.backgroundBlendMode = 'multiply';
-		//прозрачность фона
-		choosedContainer.style.backgroundColor = 'rgba(28, 28, 28, 0.6)';
-	});
+			choosedContainer.style.background = `url('/img/ploshadki/${button.id.slice(1)}.png') center center / cover no-repeat`;
+			//затемнить фон
+			choosedContainer.style.backgroundBlendMode = 'multiply';
+			//прозрачность фона
+			choosedContainer.style.backgroundColor = 'rgba(28, 28, 28, 0.6)';
+		});
+	} else {
+		button.style.maxHeight = '8rem';
+		//accordion
+		button.addEventListener('click', () => {
+			//check if containers height is already max height
+			if (button.style.maxHeight != '8rem') {
+				button.style.maxHeight = '8rem';
+				button.style.height = '100%';
+				texts[button.id.slice(1) - 1].style.display = 'none';
+			} else {
+				texts[button.id.slice(1) - 1].style.display = 'block';
+				button.style.height = 'auto';
+				button.style.maxHeight = '28.994375rem';
+			}
+		});
+	}
 });
 
 document.querySelector('.headerL__back-link').addEventListener('click', () => {
