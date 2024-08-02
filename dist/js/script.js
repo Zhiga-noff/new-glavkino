68743a64804d46e72d0969836d0242b768ea0283
Maksim <73832732+ms0ur@users.noreply.github.com>
2024-08-02 16:17:39 +0300

    Add files via upload

---
+++ dist/js/script.js
@@ -0,0 +1,97 @@
+// Функция для конвертации фотографий в формат webp
+function isWebp() {
+	function testWebP(callback) {
+		var webP = new Image();
+		webP.onload = webP.onerror = function () {
+			callback(webP.height == 2);
+		};
+		webP.src =
+			'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+	}
+	testWebP(function (support) {
+		if (support == true) {
+			document.querySelector('body').classList.add('webp');
+		} else {
+			document.querySelector('body').classList.add('no-webp');
+		}
+	});
+}
+
+isWebp();
+
+const ua = navigator.userAgent.toLowerCase();
+
+const browser = {
+	chrome: /chrome|chromium/.test(ua) && !/edg/.test(ua),
+	edge: /edg/.test(ua),
+	firefox: /firefox/.test(ua),
+	safari: /^((?!chrome|android).)*safari/.test(ua),
+	ie: /msie|trident/.test(ua),
+};
+
+console.log(browser);
+
+if (browser.safari) {
+	document.querySelector('.contact-info').classList.add('contact-info_safari');
+	document.querySelector('.services__grid').classList.add('services__grid_safari');
+	document.querySelectorAll('.services__grid-item').forEach((item) => {
+		item.classList.add('services__grid-item_safari');
+	});
+	document.querySelector('.burger').classList.add('burger_safari');
+	
+}
+
+const menu = document.querySelectorAll('.open');
+const menuClose = document.querySelectorAll('.close');
+const burger = document.querySelector('.burger');
+
+menu.forEach((item) => {
+	item.addEventListener('click', () => {
+		burger.classList.add('burger-active');
+	});
+});
+menuClose.forEach((item) => {
+	item.addEventListener('click', () => {
+		burger.classList.remove('burger-active');
+	});
+});
+
+// Данная функция позволяет создавать анимацю появления объектов при скролле сайта
+// Проверяем наличие класса у объекта который будем анимировать
+let animItems = document.querySelectorAll('.anim-items');
+// Если они есть то выполняется следущие условия
+if (animItems.length > 0) {
+	window.addEventListener('scroll', animOnScroll, true);
+	function animOnScroll() {
+		for (let index = 0; index < animItems.length; index++) {
+			const animItem = animItems[index];
+			const animItemHeight = animItem.offsetHeight;
+			const animItemOffset = offset(animItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+			if (animItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+			if (
+				window.scrollY > animItemOffset - animItemPoint &&
+				window.scrollY < animItemOffset + animItemHeight
+			) {
+				animItem.classList.add('anim');
+			} else {
+				if (!animItem.classList.contains('no-anim-again')) {
+					animItem.classList.remove('anim');
+				}
+			}
+		}
+	}
+	function offset(el) {
+		const rect = el.getBoundingClientRect();
+		let scrollLeft = window.scrollY || document.documentElement.scrollLeft;
+		let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+	}
+	animOnScroll();
+}
