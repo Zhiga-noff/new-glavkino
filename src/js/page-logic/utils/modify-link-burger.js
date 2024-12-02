829d0c424b9b92fe6cc0d68e63a57a6fccc6a558
KZHiganov <kzhiganov@zvezdamedia.ru>
2024-12-02 15:59:45 +0300

    Сделал страницу о нас

--- src/js/page-logic/utils/modify-link-burger.js
+++ src/js/page-logic/utils/modify-link-burger.js
@@ -1,7 +1,9 @@
 export const modifyLinkBurger = () => {
-	const burgerLink = document.querySelectorAll('.burger__link');
-	burgerLink.forEach((link) => {
-		const baseURL = link.attributes.href.value;
-		link.href = 'index.html' + baseURL;
-	});
+    const burgerLink = document.querySelectorAll('.burger__link');
+    burgerLink.forEach((link) => {
+        const baseURL = link.attributes.href.value;
+        if (baseURL.includes('#')) {
+            link.href = 'index.html' + baseURL;
+        }
+    });
 };
--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,30 @@
-import { isWebp } from './utils/is-web.js';
-import { smoothScroll } from './utils/smooth-scroll.js';
-import { openBurger } from './utils/open-burger.js';
-import { animation } from './animation/animation.js';
-import { pageRun } from './page-logic/page-run.js';
-import { screensaverOut } from './utils/screensaver-out.js';
-import { popUpFunc } from './utils/pop-up-func.js';
+import {isWebp} from './utils/is-web.js';
+import {smoothScroll} from './utils/smooth-scroll.js';
+import {openBurger} from './utils/open-burger.js';
+import {animation} from './animation/animation.js';
+import {pageRun} from './page-logic/page-run.js';
+import {screensaverOut} from './utils/screensaver-out.js';
+import {popUpFunc} from './utils/pop-up-func.js';
+import {modifyBurger} from "./page-logic/utils/modify-burger.js";
+import {modifyLinkBurger} from "./page-logic/utils/modify-link-burger.js";
 
 export const runAppFunc = () => {
-	isWebp();
-	openBurger();
-	animation();
+    isWebp();
+    openBurger();
+    animation();
 
-	const locationPath = window.location.pathname;
-	const pageMatch =
-		locationPath.includes('pavilioni') || locationPath.includes('ploshadki');
-	if (pageMatch) {
-		pageRun();
-	} else {
-		screensaverOut();
-		smoothScroll();
-	}
-	popUpFunc();
+    const locationPath = window.location.pathname;
+    const pageMatch =
+        locationPath.includes('pavilioni') || locationPath.includes('ploshadki');
+    if (pageMatch) {
+        pageRun();
+    }
+    if (locationPath.includes('about-us')) {
+        modifyBurger()
+        modifyLinkBurger()
+    } else {
+        screensaverOut();
+        smoothScroll();
+    }
+    popUpFunc();
 };
