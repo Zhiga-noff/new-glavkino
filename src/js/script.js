07af8df8a40790345937f498e3013e4a0204328e
kirill <kzhiganov@zvezdamedia.ru>
2024-12-06 16:03:12 +0300

    Увеличил заставку на главном экране а так же добавил ссылку студия звукозаписи

--- src/js/script.js
+++ src/js/script.js
@@ -1,29 +1,28 @@
-import {isWebp} from './utils/is-web.js';
-import {smoothScroll} from './utils/smooth-scroll.js';
-import {openBurger} from './utils/open-burger.js';
-import {animation} from './animation/animation.js';
-import {pageRun} from './page-logic/page-run.js';
-import {screensaverOut} from './utils/screensaver-out.js';
-import {popUpFunc} from './utils/pop-up-func.js';
-import {modifyBurger} from "./page-logic/utils/modify-burger.js";
-import {modifyLinkBurger} from "./page-logic/utils/modify-link-burger.js";
+import { isWebp } from './utils/is-web.js';
+import { smoothScroll } from './utils/smooth-scroll.js';
+import { openBurger } from './utils/open-burger.js';
+import { animation } from './animation/animation.js';
+import { pageRun } from './page-logic/page-run.js';
+import { popUpFunc } from './utils/pop-up-func.js';
+import { modifyBurger } from './page-logic/utils/modify-burger.js';
+import { modifyLinkBurger } from './page-logic/utils/modify-link-burger.js';
 
 export const runAppFunc = () => {
-    isWebp();
-    openBurger();
-    animation();
+	isWebp();
+	openBurger();
+	animation();
 
-    const locationPath = window.location.pathname;
-    const pageMatch =
-        locationPath.includes('pavilioni') || locationPath.includes('ploshadki');
-    if (pageMatch) {
-        pageRun();
-    } else if (locationPath.includes('about-us')) {
-        modifyBurger()
-        modifyLinkBurger()
-    } else {
-        screensaverOut();
-        smoothScroll();
-    }
-    popUpFunc();
+	const locationPath = window.location.pathname;
+	const pageMatch =
+		locationPath.includes('pavilioni') || locationPath.includes('ploshadki');
+	if (pageMatch) {
+		pageRun();
+	} else if (locationPath.includes('about-us')) {
+		modifyBurger();
+		modifyLinkBurger();
+	} else {
+		// screensaverOut();
+		smoothScroll();
+	}
+	popUpFunc();
 };
