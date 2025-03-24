414cca7343b5a45aac6b33552a64c1209fdc5fd2
KZHiganov <kzhiganov@zvezdamedia.ru>
2025-03-24 11:46:29 +0300

    Поправил немного стили

--- src/js/script.js
+++ src/js/script.js
@@ -6,32 +6,32 @@ import {pageRun} from './page-logic/page-run.js';
 import {popUpFunc} from './utils/pop-up-func.js';
 import {modifyBurger} from './page-logic/utils/modify-burger.js';
 import {modifyLinkBurger} from './page-logic/utils/modify-link-burger.js';
-import videojs from "video.js";
+import videojs from 'video.js';
 
 export const runAppFunc = () => {
-
-    const player = videojs('vid1', {
-        controls: true,
-        autoplay: false,
-        preload: 'auto',
-        loop: false,
-        // aspectRatio: '16:9'
-    });
-
-
+    let player;
     isWebp();
     openBurger();
     animation();
 
     const locationPath = window.location.pathname;
     const pageMatch =
-        locationPath.includes('pavilioni') || locationPath.includes('ploshadki') || locationPath.includes('studia');
+        locationPath.includes('pavilioni') ||
+        locationPath.includes('ploshadki') ||
+        locationPath.includes('studia');
     if (pageMatch) {
         pageRun();
     } else if (locationPath.includes('about-us')) {
         modifyBurger();
         modifyLinkBurger();
     } else {
+        player = videojs('vid1', {
+            controls: true,
+            autoplay: false,
+            preload: 'auto',
+            loop: false,
+            // aspectRatio: '16:9'
+        });
         // screensaverOut();
         smoothScroll();
     }
